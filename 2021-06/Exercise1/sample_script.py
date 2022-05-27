8d664e59a198493da757366837a553db4cb4f383
Manasvita Joshi <github@manasvita.com>
2022-05-27 13:16:26 -0400

    Moved the June 2021 Discovery Basics training material to this folder

---
+++ 2021-06/Exercise1/sample_script.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.zeros((5,5))   # Create an array of all zeros
+print(arr)
+
+ident = np.eye(2)       # Create a 2x2 identity matrix
+print(ident)
+
+randArr = np.random.random((2,2))  # Create an array filled with random values
+print(randArr)
+
+
