f6debafadabccf051889928243ed2420d93715f6
Joseph Cooke <j.cooke@northeastern.edu>
2023-02-28 05:48:14 -0500

    Update for general training

--- Exercise3/sample_array_job.bash
+++ Exercise3/sample_array_job.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=reservation
-#SBATCH --reservation=fall_training_cpu_2022 
+#SBATCH --partition=short
 #SBATCH --job-name=test_array
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
@@ -8,10 +7,6 @@
 #SBATCH --output=%A_%a.output
 #SBATCH --error=%A_%a.error
 #SBATCH --array=1-4%4 #run a 4-job array, 4 jobs will launched at the same time.
-
-# Use a regular partition, such as express, when not in a training
-# session
-##SBATCH --partition=express
 
 echo "Job array ID: $SLURM_ARRAY_JOB_ID , sub-job $SLURM_ARRAY_TASK_ID is running!"
 
