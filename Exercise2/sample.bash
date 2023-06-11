338d0904ca5298d838cb6d715b00ed9074ce3de4
Keval <kevalparesh@gmail.com>
2023-06-11 05:09:23 -0400

    Modified Exercise 2 and 3

--- Exercise2/sample.bash
+++ Exercise2/sample.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
-#SBATCH --partition=short
+#SBATCH --partition=reservation
+#SBATCH --reservation=bootcamp_cpu_2023
 #SBATCH --job-name=test
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=%j.output
 #SBATCH --error=%j.error
+
+#####Use after training##############
+##SBATCH -p short
 
 echo "HELLO WORLD!"
